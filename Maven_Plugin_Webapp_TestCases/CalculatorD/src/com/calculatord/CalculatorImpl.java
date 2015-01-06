8f7c4d07f1fa3c737804756723298096b23a88e7
PoonamWebonise <poonam.patare@weboniselab.com>
2015-01-06 20:01:10 +0530

    Maven Plugin Simple Test Cases

---
+++ Maven_Plugin_Webapp_TestCases/CalculatorD/src/com/calculatord/CalculatorImpl.java
@@ -0,0 +1,23 @@
+package com.calculatord;
+
+public class CalculatorImpl implements Calculator {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+//		System.out.println("Calculator of B implemented");
+	}
+
+	public void addition(int x, int y) {
+		// TODO Auto-generated method stub
+		System.out.println("Addition is: "+(x+y));
+		System.out.println("Method Calculator of B implemented");
+	}
+
+	public void substraction(int x, int y) {
+		// TODO Auto-generated method stub
+		System.out.println("Substraction is: "+(x-y));
+	}
+
+}
