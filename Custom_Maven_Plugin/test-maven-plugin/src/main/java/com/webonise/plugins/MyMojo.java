86262b31cc54f55eaac85bdd70f4cd10ecc27186
WeboniseLab <tanvi.vanmali@weboniselab.com>
2015-01-08 19:54:06 +0530

    reading the contents from POM.xml

--- Custom_Maven_Plugin/test-maven-plugin/src/main/java/com/webonise/plugins/MyMojo.java
+++ Custom_Maven_Plugin/test-maven-plugin/src/main/java/com/webonise/plugins/MyMojo.java
@@ -16,94 +16,110 @@ package com.webonise.plugins;
  * limitations under the License.
  */
 
-
 import java.io.File;
 import java.io.FileFilter;
-
+import java.io.FileReader;
+import java.util.*;
+import org.apache.maven.model.Dependency;
+import org.apache.maven.model.Model;
+import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.project.MavenProject;
 
-
 /**
  * Goal which touches a timestamp file.
- *
+ * 
  * @goal touch
  * 
  * @phase process-sources
  */
-public class MyMojo
-    extends AbstractMojo
-{
+public class MyMojo extends AbstractMojo {
 	/**
-     * Location of the file.
-     * 
-     * @parameter default-value="${project}"
-     * @required
-     */
-    MavenProject project;
-    /**@parameter default-value="${localRepository}" */
-    org.apache.maven.artifact.repository.ArtifactRepository localRepository;
-    public void listf( File dir)
-    {
+	 * Location of the file.
+	 * 
+	 * @parameter default-value="${project}"
+	 * @required
+	 */
+	MavenProject project;
+	/** @parameter default-value="${localRepository}" */
+	org.apache.maven.artifact.repository.ArtifactRepository localRepository;
 
-        File[] fList = dir.listFiles();
-        for (File file1 : fList)
-        {
-            if (file1.isFile()) 
-            {
-            	if(file1.getName().endsWith(".pom"))
-    			{
-    				getLog().info("pom file:"+file1);
-    			}
-            	//getLog().info("it is a file"+file1);
-            } else if (file1.isDirectory())
-            {
-            	listf(file1);
-            }
-        }
-    }
-   
-    public void execute() throws MojoExecutionException,MojoFailureException 
-    {
-        getLog().info("this is not coming");
-        getLog().info("Project Version: " + project.getVersion().toString());
-        getLog().info("Artifact ID " + project.getArtifactId().toString());
-       
-    	
-        getLog().info("LocalRepository Path:"+localRepository.getBasedir());
-        
-        String path=localRepository.getBasedir().concat("/");
-
-        File f = new File(path);
-        FileFilter directoryFilter = new FileFilter()
-        {
-			public boolean accept(File file) 
-			{
-				return file.isDirectory();
-			}
-        };
-		File[] files = f.listFiles(directoryFilter);
-		
-		for (File file1 : files) 
-		{
-			if (file1.isDirectory())
-			{
-				getLog().info("directory:"+file1);
-				this.listf(file1);
-			} 
-			//else if(file1.getName().endsWith(".pom"))
-			//{
-			//	getLog().info("pom file:"+file1);
-			//}
-			else
-			{
-				
+	public void listf(File dir) {
+		File[] fList = dir.listFiles();
+		for (File file1 : fList) {
+			if (file1.isFile()) {
+				if (file1.getName().endsWith(".pom")) {
+					getLog().info("pom file:" + file1);
+					this.getPomObject(file1);
+				}
+			} else if (file1.isDirectory()) {
+				listf(file1);
 			}
 		}
-     
-    }
-    
-    
+	}
+
+	public void getPomObject(File pomfile) {
+
+		MavenProject project;
+
+		Model model = null;
+		FileReader reader = null;
+		MavenXpp3Reader mavenreader = new MavenXpp3Reader();
+
+		try {
+			reader = new FileReader(pomfile);
+			model = mavenreader.read(reader);
+			model.setPomFile(pomfile);
+		} catch (Exception ex) {
+		}
+
+		project = new MavenProject(model);
+		project.getProperties();
+		getLog().info("this is not coming");
+		getLog().info("Project Version: " + project.getVersion().toString());
+		getLog().info("Project Dependencies: ");
+		List<Dependency> dependencies = project.getDependencies();
+		Iterator<Dependency> myIterator = dependencies.iterator();
+		String artifact = "";
+		String version = "";
+		while (myIterator.hasNext()) {
+			Dependency current = myIterator.next();
+			getLog().info(
+					"Artifact ID: " + current.getArtifactId() + "\tVersion: "
+							+ current.getVersion());
+			artifact = current.getArtifactId();
+			version = current.getVersion();
+		}
+		getLog().info("Current Artifact ID: " + artifact);
+	}
+
+	public void execute() throws MojoExecutionException, MojoFailureException {
+		getLog().info("this is not coming");
+		getLog().info("Project Version: " + project.getVersion().toString());
+		getLog().info("Artifact ID " + project.getArtifactId().toString());
+		getLog().info("LocalRepository Path:" + localRepository.getBasedir());
+
+		String path = localRepository.getBasedir().concat("/");
+
+		File f = new File(path);
+		FileFilter directoryFilter = new FileFilter() {
+			public boolean accept(File file) {
+				return file.isDirectory();
+			}
+		};
+		File[] files = f.listFiles(directoryFilter);
+
+		for (File file1 : files) {
+			if (file1.isDirectory()) {
+
+				this.listf(file1);
+			} else {
+
+			}
+		}
+
+	}
+
 }
