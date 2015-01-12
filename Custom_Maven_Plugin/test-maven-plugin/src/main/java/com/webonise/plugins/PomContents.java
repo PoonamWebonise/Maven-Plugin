e0ba2e02e7a9b77ad4a831fc721f8ea2f51aa7ac
ketansWebonise <ketan.saxena@gmail.com>
2015-01-12 12:15:12 +0530

    Architechture changed. Unnecessary parameters in methods removed.  Class VersionResolver removed

--- Custom_Maven_Plugin/test-maven-plugin/src/main/java/com/webonise/plugins/PomContents.java
+++ Custom_Maven_Plugin/test-maven-plugin/src/main/java/com/webonise/plugins/PomContents.java
@@ -4,6 +4,9 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+import org.apache.maven.model.Dependency;
 import org.apache.maven.model.Model;
 import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
 import org.apache.maven.project.MavenProject;
@@ -31,13 +34,13 @@ public class PomContents extends AbstractMojo {
 	/** @parameter default-value="${localRepository}" */
 	org.apache.maven.artifact.repository.ArtifactRepository localRepository;
 	
-	
+	public static String currentArtifact;
+	public static String currentVersion;
 	public void execute() throws MojoExecutionException, MojoFailureException {
 		getLog().info("Printing Current Project's Artifact ID & Version");
 		getLog().info("Project Version: " + project.getVersion().toString());
 		getLog().info("Artifact ID " + project.getArtifactId().toString());
 		getLog().info("LocalRepository Path:" + localRepository.getBasedir());
-		System.out.println("***************************"+project.getVersion());
 		
 		File repository = new File(localRepository.getBasedir().concat("/"));
 		try {
@@ -47,43 +50,58 @@ public class PomContents extends AbstractMojo {
 			e.printStackTrace();
 		}
 		PomFileFinder pomfile = new PomFileFinder();
-		pomfile.findSubDirectories(repository,project.getArtifactId(),project.getVersion());
-		
+		currentArtifact=project.getArtifactId();
+		currentVersion=project.getVersion();
+		pomfile.findSubDirectories(repository);
+
 	}
 
 	// accepting the pom file and checking current project's artifact id with
 	// all the artifact id's found in pom
-	public void getPomObject(File pomfile, String currentProjectArtifact, String currentProjectVersion) {
+	public void getPomObject(File pomfile) throws IOException {
 		
+		
+
 		try {
-			//getting Model for the passed pom file
 			Model model = new MavenXpp3Reader().read(new FileReader(pomfile));
 			model.setPomFile(pomfile);
-			
-			//Getting the MavenProject from the Model
+
 			project = new MavenProject(model);
 			project.getProperties();
-			
-			//Calling function to resolve version of dependancy and the current project
-			VersionResolver versionResolver = new VersionResolver();
-			versionResolver.resolveDependencyVersion(project, getLog());
-			
-		} 
+			@SuppressWarnings("unchecked")
+			List<Dependency> dependencies = project.getDependencies();
+			Iterator<Dependency> dependencyIterator = dependencies.iterator();
+			while (dependencyIterator.hasNext()) {
+				Dependency current = dependencyIterator.next();
+				String artifact = current.getArtifactId();
+				String version = current.getVersion();
+				if (artifact.equals(currentArtifact)) {
+					getLog().info("pom file:" + pomfile);
+					getLog().info(
+							"Dependency Artifact ID: " + artifact
+									+ "\tVersion: " + version);
+					if (!currentVersion.equalsIgnoreCase(version)) {
+
+						getLog().error(
+								"DEPENDENCY VERSION MISMATCH in one of the Dependants. Please update xml and retry.");
+					} else
+						getLog().info(
+								"Current Version: " + currentVersion
+										+ "Version found in pom: " + version);
+				}
+			}
+		}
+		
 		catch (FileNotFoundException ex)
 		{
 			getLog().error("pom File not found");
 		}
-		catch (IOException e)
-		{
-			e.printStackTrace();
+		catch (XmlPullParserException e) {
+			
+			//e.printStackTrace();
+			//getLog().info("skipping incompatible pom files....");
 		}
-		catch (XmlPullParserException e)
-		{
-			e.printStackTrace();
-		}
-		catch(Exception ex)
-		{
-			ex.printStackTrace();
-		}
+		
 	}
+
 }
--- Custom_Maven_Plugin/test-maven-plugin/src/main/java/com/webonise/plugins/PomFileFinder.java
+++ Custom_Maven_Plugin/test-maven-plugin/src/main/java/com/webonise/plugins/PomFileFinder.java
@@ -2,14 +2,13 @@ package com.webonise.plugins;
 
 import java.io.File;
 import java.io.FileFilter;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.IOException;
 
 public class PomFileFinder{
 
 	// accepting .m2 path, opening the repository, storing all names of
 	// sub-folders in a file array
-	public void findSubDirectories(File repository, String artifactID, String version) {
+	public void findSubDirectories(File repository) {
 
 		
 		//filter returning files only when they're directories
@@ -20,28 +19,33 @@ public class PomFileFinder{
 		};
 		
 		for (File currentFile : repository.listFiles(directoryFilter)){
-			this.scanAllFiles(currentFile, artifactID, version);
+			if (currentFile.isDirectory())
+			{
+				this.listAllFiles(currentFile);
+			}
 		}
 	}
 
 	// scanning the sub-folders and finding .pom file
-	public void scanAllFiles(File subDirectory, String artifactID, String version) {
-		List<File> allPomFiles = new ArrayList<File>();
+	public void listAllFiles(File subDirectory) {
 		PomContents pomContents = new PomContents();
 		for (File eachFile : subDirectory.listFiles()) {
 			if (eachFile.isFile())
 			{
 				if (eachFile.getName().endsWith(".pom"))
 				{
-					pomContents.getPomObject(eachFile, artifactID,version);
-					
+					try {
+						pomContents.getPomObject(eachFile);
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
 				}
 			}
 			else
 			{
-				scanAllFiles(eachFile, artifactID, version);
+				listAllFiles(eachFile);
 			}
 		}
-		
 	}
 }
